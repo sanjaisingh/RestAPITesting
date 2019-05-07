37e5b1b497c9242e2e7f0e35eec3054e80f19236
Sanjay Singh <Sanjay Singh@DESKTOP-HV678ES>
2019-05-07 20:31:16 +0530

    Assertion

--- restAssuredFramework/src/test/java/getRequest/GetDataStatic.java
+++ restAssuredFramework/src/test/java/getRequest/GetDataStatic.java
@@ -1,20 +1,23 @@
 package getRequest;
 
-import org.codehaus.groovy.transform.EqualsAndHashCodeASTTransformation;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
 
-import static io.restassured.RestAssured.*;
-import io.restassured.response.Response;
+import org.testng.annotations.Test;
 
 public class GetDataStatic {
 	
 	@Test
 	public void getResponseCode()
 	{
-
 		String URL = "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22";
-		//get(URL).then().body("weather", equals(3));
-		;
+
+		given().
+		when().
+			get(URL).
+		then().
+			assertThat().                                                                                                                                                                         			
+			statusCode(200).
+			and().
+			header("connection", "keep-alive");
 	}
 }
